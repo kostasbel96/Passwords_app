41d2187fd831a07a892559c487d639a5f26fdb99
Kostas Veloutsos <kwstasvel96@gmail.com>
2023-01-07 17:12:23 +0200

    7/1/2023

--- my_app.py
+++ my_app.py
@@ -1,35 +1,42 @@
 import pickle
 
+class Type:
+
+    def __init__(self, type_name):
+        self.type_name = type_name
+
 class Person:
 
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
 
-class Account:
+class Account(Type):
 
-    def __init__(self, person, username_or_email, password):
+    def __init__(self, account_type, person, username_or_email, password):
+        super().__init__(account_type)
         self.person = person
         self.username_or_email = username_or_email
         self.password = password
 
-    def __str__(self):
-        return f"Person: {self.person.name} {self.person.surname}\nUsername or Email: {self.username_or_email}\nPassword: {self.password}"   
+    # def __str__(self):
+        # return f"Person: {self.person.name} {self.person.surname}\nUsername or Email: {self.username_or_email}\nPassword: {self.password}"   
 
 list_accounts = []
 
-reply = input("Enter:")
-if reply == "1":
-    try:
-        with open("data.pkl","wb") as f:
-            person = Person("ete","test")
-            pickle.dump(person,f)
-    except:
-        print("Error")
-else:
-    try:
-        with open("data.pkl","rb") as f:
-            ob = pickle.load(f)
-            print(ob.name)
-    except:
-        print("ERROR")        
+# list_accounts.append(Account("facebook", Person("Kostas", "Veloutsos"), "kwstasvel96@gmail.com", "password"))
+
+ 
+# try:
+#     with open("test.pkl","wb") as f:
+#         pickle.dump(list_accounts,f)
+# except:
+#     print("Error")
+
+try:
+    with open("test.pkl","rb") as f:
+        list_accounts = pickle.load(f)
+except:
+    print("Error")
+
+print(list_accounts[0].type_name)            
